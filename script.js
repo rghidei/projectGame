63d2cb26415191e523e7c245744df92cd09e7f10
Rodas Ghidei <rodasghidei@yahoo.com>
2021-08-05 21:05:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//1st dealer card - cant show
+let d1ValTop = document.querySelector('.d1ValT')
+let d1SuitTop = document.querySelector('.d1SuitT')
+let d1Suit = document.querySelector('.d1Suit')
+let d1ValBot = document.querySelector('.d1ValB')
+let d1SuitBot = document.querySelector('.d1SuitB')
+
+//2nd dealer card
+let d2ValTop = document.querySelector('.d2ValT')
+let d2SuitTop = document.querySelector('.d2SuitT')
+let d2Suit = document.querySelector('.d2Suit')
+let d2ValBot = document.querySelector('.d2ValB')
+let d2SuitBot = document.querySelector('.d2SuitB')
+
+//1st player card
+let p1ValTop = document.querySelector('.p1ValT')
+let p1SuitTop = document.querySelector('.p1SuitT')
+let p1Suit = document.querySelector('.p1Suit')
+let p1ValBot = document.querySelector('.p1ValB')
+let p1SuitBot = document.querySelector('.p1SuitB')
+
+//2nd player card
+let p2ValTop = document.querySelector('.p2ValT')
+let p2SuitTop = document.querySelector('.p2SuitT')
+let p2Suit = document.querySelector('.p2Suit')
+let p2ValBot = document.querySelector('.p2ValB')
+let p2SuitBot = document.querySelector('.p2SuitB')
